09a6c1c6c835420bb383286f47afff02a124e8ba
acggithub <54444075+acggithub@users.noreply.github.com>
2021-07-27 12:21:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var vid = document.getElementById("myVideo"); 
+//var content= document.getElementById("content");
+function playVid() { 
+  vid.play();
+ } 
+
+function pauseVid() { 
+  vid.pause(); 
+} 
